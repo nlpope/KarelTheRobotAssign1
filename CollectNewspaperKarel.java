483eac75aec00e03dbb68eadf203953bd5e21c96
Noah Pope <npope@tutanota.com>
2026-06-10 12:03:39 -0500

    init commitz

---
+++ CollectNewspaperKarel.java
@@ -0,0 +1,28 @@
+/*
+ * File: CollectNewspaperKarel.java
+ * --------------------------------
+ * At present, the CollectNewspaperKarel subclass does nothing.
+ * Your job in the assignment is to add the necessary code to
+ * instruct Karel to walk to the door of its house, pick up the
+ * newspaper (represented by a beeper, of course), and then return
+ * to its initial position in the upper left corner of the house.
+ */
+
+import stanford.karel.*;
+
+public class CollectNewspaperKarel extends SuperKarel {
+	
+	public void run() 
+	{
+		move();
+		turnRight();
+		move(); 
+		turnLeft();
+		move();
+		move();
+		pickBeeper();
+	}
+	
+	//private static final long serialVersionUID = 7526472295622776147L;  // unique id
+
+}
---
+++ StoneMasonKarel.java
@@ -0,0 +1,41 @@
+/*
+ * File: StoneMasonKarel.java
+ * --------------------------
+ * The StoneMasonKarel subclass as it appears here does nothing.
+ * When you finish writing it, it should solve the "repair the quad"
+ * problem from Assignment 1.  In addition to editing the program,
+ * you should be sure to edit this comment so that it no longer
+ * indicates that the program does nothing.
+ */
+
+import stanford.karel.*;
+
+public class StoneMasonKarel extends SuperKarel {
+	
+	public void run()
+	{
+		fillColumn();
+	}
+
+	public void fillColumn()
+	{
+		if (leftIsClear()) { turnLeft(); }
+		else { turnRight(); }
+		
+		while (frontIsClear()) {
+			if (noBeepersPresent()) { putBeeper(); }
+			move();
+		}
+		if (noBeepersPresent()) { putBeeper(); }
+		arreglar();
+	}
+	
+	private void arreglar() 
+	{
+		if (facingNorth()) { turnRight(); } 
+		else { turnLeft(); }
+		if (frontIsBlocked()){ print("done"); return; }
+		for (int i = 0; i < 4; i++) { move(); }
+		fillColumn();
+	}
+}
